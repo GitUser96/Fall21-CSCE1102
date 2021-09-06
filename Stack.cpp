a0aaaae8c4c2e7337d5fbf5e33a88b27377e2419
User96 <maiswu.1996@gmail.com>
2021-09-06 11:38:38 +0200

    Uploading Files

---
+++ Stack.cpp
@@ -0,0 +1,38 @@
+#include "Stack.h"
+
+Stack::Stack()
+{
+	top = -1;
+}
+bool Stack::push(char value)
+{
+	if (top >= (CAPACITY - 1))
+		return false;
+	else
+	{
+		top++;
+		container[top] = value;
+		return true;
+	}
+}
+char Stack::pop()
+{
+	if (top < 0)
+		return '?';
+	else
+	{
+		char x = container[top];
+		top--;
+		return x;
+	}
+}
+bool Stack::empty()
+{
+	return (top < 0);
+}
+void Stack::print()
+{
+	for (int i = 0; i <= top; i++)
+		cout << container[i] << " ";
+	cout << endl;
+}
---
+++ Stack.h
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+const int CAPACITY = 100;
+
+class Stack
+{
+private:
+	int top;
+	char container[CAPACITY];
+public:
+	Stack();
+	bool push(char);
+	char pop();
+	bool empty();
+	void print();
+};
